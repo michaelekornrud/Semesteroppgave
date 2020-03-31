8cd76037df6a03c41c9300a91f7f7ea51b614fd7
MIchael Ekornrud <s341866@oslomet.no>
2020-03-31 16:54:13 +0200

    Legg til komponent-knapp lagt til

--- Semesteroppg/src/PCBygging/controller.java
+++ Semesteroppg/src/PCBygging/controller.java
@@ -1,4 +1,34 @@
 package PCBygging;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class controller {
+
+    @FXML
+    private Button btnLeggTil;
+
+    @FXML
+    void addComponent (ActionEvent event) throws IOException {
+        try {
+            Parent PCByggingParent = FXMLLoader.load(getClass().getClassLoader().getResource("ProductWindow/productWindow.fxml"));
+            Scene PCByggingScene = new Scene(PCByggingParent);
+
+            //Denne linjen henter stage info
+            Stage PCWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
+            PCWindow.setScene(PCByggingScene);
+            PCWindow.show();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
