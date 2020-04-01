5872ad2469003e063e889a6cb763ed0329b74c3a
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2020-04-01 18:10:26 +0200

    Har lagt til at man kan redigere de ulike objektene i tableview.

--- Semesteroppg/src/Exceptions/ProductValidator.java
+++ Semesteroppg/src/Exceptions/ProductValidator.java
@@ -1,5 +1,11 @@
 package Exceptions;
 
+import ProductWindow.Controller;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ProductValidator {
 
     public static String testProductName(String name) throws InvalidProductNameException{
@@ -37,12 +43,29 @@ public class ProductValidator {
         return price;
     }
 
-    public static String testProductNumber(String number) throws InvalidProductNumberException{
+    public static String testProductNumber(String number /*String test*/) throws InvalidProductNumberException{
+
+        //Metode for å teste om prduktnr er unikt!
+        // Trenger å koble denne sammen med innholdet i coice-boksene for å sjekke om nummeret er unikt.
+
+        /*if (test.contains(number)){
+            throw new InvalidProductNumberException("Error! The productNr already exists.");
+        }*/
+
         if (!number.matches("([a-zæøåA-ZÆØÅ]{2})+([0-9]{8})")){
             throw new InvalidProductNumberException("Invalid input in productnumber-field.\nYou must follow this layout: xx12345678 (2 letters first, then 8 numbers)");
         }
         return number;
     }
+
+
+
+
+
+
+
+
+
 
 
 }
--- Semesteroppg/src/ProductWindow/Controller.java
+++ Semesteroppg/src/ProductWindow/Controller.java
@@ -1,7 +1,7 @@
 package ProductWindow;
 
+
 import Exceptions.ProductValidator;
-import javafx.beans.property.DoubleProperty;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,16 +10,15 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-
-import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class Controller{
 
+
+public class Controller{
 
     @FXML
     private TextField txtProductName;
@@ -40,14 +39,49 @@ public class Controller{
     private TextField txtPrice;
 
     @FXML
+    private TableColumn<Product, String> colName;
+
+    @FXML
+    private TableColumn<Product, String> colProdNr;
+
+    @FXML
+    private TableColumn<Product, String> colNumberOfProduct;
+
+    @FXML
+    private TableColumn<Product, String> colType;
+
+    @FXML
+    private TableColumn<Product, String> colBrand;
+
+    @FXML
+    private TableColumn<Product, String> colPrice;
+
+    @FXML
     private TableView<Product> tableView;
     private ProductRegister newObjects = new ProductRegister();
 
+
+    @FXML
+    public void initialize(){
+        newObjects.attachToTableView(tableView);
+        tableView.setEditable(true);
+        colName.setCellFactory(TextFieldTableCell.forTableColumn());
+        colProdNr.setCellFactory(TextFieldTableCell.forTableColumn());
+        colType.setCellFactory(TextFieldTableCell.forTableColumn());
+        colBrand.setCellFactory(TextFieldTableCell.forTableColumn());
+        //colNumberOfProduct.setCellFactory(TextFieldTableCell.forTableColumn());
+
+
+        //colPrice.setCellFactory(TextFieldTableCell.forTableColumn());
+
+    }
+
     @FXML
     void btnAdd(ActionEvent event) {
+        initialize();
         Product newProduct = createProductObjectFromGUI();
         ProductRegister.addElement(newProduct);
-        newObjects.attachToTableView(tableView);
+        resetTxtFields();
 
     }
 
@@ -91,13 +125,88 @@ public class Controller{
         String stringPrice = txtPrice.getText();
         double price = Double.parseDouble(stringPrice);
 
-        /*return new Product(name, productNumber, numberOfProducts, type, brand, price);*/
+        /*String test = "HK12345678"; //En test for å sjekke om produktnr er unikt. Denne stringen må slettes og endres til tidligere innhold i tableview*/
 
+       /*String[] prodNr = new String[]{productNumber};
+        List<String> list = Arrays.asList(prodNr);
+        if (list.contains(productNumber)){
+            throw new InvalidProductNumberException("The number already exists!");
+        }*/
 
-        return new Product(ProductValidator.testProductName(name), ProductValidator.testProductNumber(productNumber),
+            return new Product(ProductValidator.testProductName(name),ProductValidator.testProductNumber(productNumber),
                 ProductValidator.testNumberOfProducts(numberOfProducts),ProductValidator.testProductType(type),
                 ProductValidator.testProductBrand(brand),ProductValidator.testPrice(price));
 
     }
+
+    @FXML
+    public void editTableview_Name(TableColumn.CellEditEvent<Product, String> edit){
+        Product prod = tableView.getSelectionModel().getSelectedItem();
+        String name = edit.getNewValue();
+        ProductValidator.testProductName(name);
+        prod.setTxtProductName(name);
+        tableView.refresh();
+    }
+
+
+    @FXML
+    public void editTableview_prodNr(TableColumn.CellEditEvent<Product, String> edit){
+        Product prod = tableView.getSelectionModel().getSelectedItem();
+        String prodNr = edit.getNewValue();
+        ProductValidator.testProductNumber(prodNr);
+        prod.setTxtProductNumber(prodNr);
+        tableView.refresh();
+
+    }
+
+    @FXML //Må fikses på!! Fungerer ikke
+    public void editTableview_NumberOfProducts(TableColumn.CellEditEvent<Product, String> edit){
+        Product prod = tableView.getSelectionModel().getSelectedItem();
+        String number = edit.getNewValue();
+        int intNumber = Integer.parseInt(number);
+        ProductValidator.testNumberOfProducts(intNumber);
+        prod.setTxtNumberOfProducts(intNumber);
+        tableView.refresh();
+    }
+
+    @FXML
+    public void editTableview_Type(TableColumn.CellEditEvent<Product, String> edit){
+        Product prod = tableView.getSelectionModel().getSelectedItem();
+        String type = edit.getNewValue();
+        ProductValidator.testProductType(type);
+        prod.setTxtType(type);
+        tableView.refresh();
+    }
+
+
+    @FXML
+    public void editTableview_Brand(TableColumn.CellEditEvent<Product, String> edit){
+        Product prod = tableView.getSelectionModel().getSelectedItem();
+        String brand = edit.getNewValue();
+        ProductValidator.testProductBrand(brand);
+        prod.setTxtBrand(brand);
+        tableView.refresh();
+    }
+
+    @FXML  //Må fikses på!! Fungerer ikke
+    public void editTableview_Price(TableColumn.CellEditEvent<Product, String> edit){
+        Product prod = tableView.getSelectionModel().getSelectedItem();
+        String price = edit.getNewValue();
+        double doubPrice = Double.parseDouble(price);
+        ProductValidator.testPrice(doubPrice);
+        prod.setTxtPrice(doubPrice);
+        tableView.refresh();
+    }
+
+    @FXML
+    private void resetTxtFields() {
+        txtProductName.setText("");
+        txtProductNumber.setText("");
+        txtNumberOfProducts.setText("");
+        txtType.setText("");
+        txtBrand.setText("");
+        txtPrice.setText("");
+    }
+
 }
 
