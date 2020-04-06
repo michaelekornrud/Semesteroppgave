f02364b14ff6a112c4df5c3ac67ac161e62a6e99
MIchael Ekornrud <s341866@oslomet.no>
2020-04-06 14:08:39 +0200

    Test package

---
+++ Semesteroppg/src/Test/TestTVinput.java
@@ -0,0 +1,17 @@
+package Test;
+
+import javax.swing.text.TableView;
+
+public class TestTVinput {
+
+
+    TableView tableView = new TableView();
+
+    if (choiceBox = kabinett){
+        add to kabinet folder;
+    }
+
+    else if (chioceBox = skjermkort) {
+        add to skjermkort folder;
+    }
+}
